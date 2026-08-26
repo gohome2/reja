10c29211716fccfc9d68039d9db7b3fef9ed2a3b
gohome2 <threed484@gmail.com>
2026-08-26 20:36:41 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,37 @@
+console.log("Web Serverni boshlash");
+
+// Express 5 uses Object.hasOwn, which is built into Node 18 and newer.
+// Keep the app working on older Node versions as well.
+if (!Object.hasOwn) {
+  Object.hasOwn = (object, property) =>
+    Object.prototype.hasOwnProperty.call(object, property);
+}
+
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+app.get("/hello", function (req, res) {
+  res.end(`<h1 style="background-color: orange">HELLO WORLD</h1>`);
+});
+
+app.get("/gift", function (req, res) {
+  res.end(`<h1>Siz sovgalar sahifasidasiz</h1>`);
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`The server is running succesfully on port: ${PORT}`);
+});
