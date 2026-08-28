b930ddd66d61e82f3ab7fcc4f4be41027cad639a
gohome2 <threed484@gmail.com>
2026-08-28 18:51:12 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ if (!Object.hasOwn) {
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -22,12 +32,17 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="background-color: orange">HELLO WORLD</h1>`);
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end(`<h1>Siz sovgalar sahifasidasiz</h1>`);
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
