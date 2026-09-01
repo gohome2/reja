a3e1acdaba3e037413b077ce173e613f27b9c93b
gohome2 <threed484@gmail.com>
2026-09-01 22:00:27 +0900

    freat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
 });
 
 //MongoDB connect
-
 //MongoDB chaqirish
 const db = require("./server.js").db();
 
@@ -37,6 +36,16 @@ app.set("view engine", "ejs");
 
 // 4: Routing code
 app.post("/create-item", (req, res) => {
+  console.log("user entered /create-item");
+  const new_reja = req.body.item;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("sucsessfully added");
+    }
+  });
   // console.log(req.body);
   // res.json({ test: "success" });
   //TODO: code with db here
@@ -47,7 +56,17 @@ app.post("/create-item", (req, res) => {
 // });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        res.render("reja", { items: data });
+      }
+    });
 });
 
 module.exports = app;
