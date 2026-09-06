b9abf8a6444e46a6074905ba5f9c5b8838519b3e
gohome2 <threed484@gmail.com>
2026-09-06 20:02:54 +0900

    feat: Frontend javascriptni qurish

--- app.js
+++ app.js
@@ -39,12 +39,8 @@ app.post("/create-item", (req, res) => {
   console.log("user entered /create-item");
   const new_reja = req.body.item;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("sucsessfully added");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
   // console.log(req.body);
   // res.json({ test: "success" });
