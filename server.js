03c8b7377e41e534234022ccd4515bad5cac22a4
gohome2 <threed484@gmail.com>
2026-09-15 20:58:35 +0900

    fix:..

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongodb.connect(
       module.exports = client;
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 1234;
       server.listen(PORT, function () {
         console.log(
           `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`,
